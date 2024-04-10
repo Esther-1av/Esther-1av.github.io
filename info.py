95ade9ecd1488c97f4fe0dcc376c438de5ba23d0
esther <157632880+Esther-1av@users.noreply.github.com>
2024-04-10 11:46:07 -0400

    join.py

--- info.py
+++ info.py
@@ -8,30 +8,33 @@ cursor = conn.cursor()
 # Create the table #
 # this is a very unorginized and unoredered sql database and needs to be cleaned up by someone who understands sql better #
 
+
 cursor.execute(
-    '''CREATE TABLE IF NOT EXISTS email (id INTEGER PRIMARY KEY, email TEXT)'''
-)
+  '''create table if not exists name (id INTEGER PRIMARY KEY, name TEXT)''')
 cursor.execute(
-    '''CREATE TABLE IF NOT EXISTS hobbies (id INTEGER PRIMARY KEY, hobbies TEXT)'''
-)
+    '''CREATE TABLE IF NOT EXISTS email (id INTEGER PRIMARY KEY, email TEXT)''')
 cursor.execute(
-    '''create table if not exists religion (id integer primary key, religion text)'''
-)
+  '''create table if not exists phone (id INTEGER PRIMARY KEY, phone INTEGER)''')
 cursor.execute(
-    '''create table if not exists age (id integer primary key, age text)''')
+  '''create table if not exists age (id INTEGER PRIMARY KEY, age INTEGER)''')
 cursor.execute(
-    '''create table if not exists sex (id integer primary key, sex text)''')
+  '''create table if not exists ethnicity (id INTEGER PRIMARY KEY, ethnicity TEXT)''')
 cursor.execute(
-    '''create table if not exists gender (id integer primary key, gender text)'''
-)
+    '''create table if not exists gender (id INTEGER PRIMARY KEY, gender TEXT)''')
 cursor.execute(
-    '''create table if not exists name (id integer primary key, name text)''')
+  '''create table if not exists sex (id INTEGER PRIMARY KEY, sex TEXT)''')
 cursor.execute(
-    '''create table if not exists phone (id integer primary key, phone text)''')
+    '''CREATE TABLE IF NOT EXISTS sexuality (id INTEGER PRIMARY KEY, sexuality TEXT)''')
+cursor.execute(
+    '''CREATE TABLE IF NOT EXISTS hobbies (id INTEGER PRIMARY KEY, hobbies TEXT)''')
+cursor.execute(
+    '''create table if not exists religion (id INTEGER PRIMARY KEY, religion TEXT)''')
+
 
 # Get the information from the user #
 
 namecorrect = "no"
+terminate = "false"
 while namecorrect == "no":
   name = input("What name should we refer to you as: ")
   namecorrect = input("Is this correct (yes, no): " + name + ": ")
@@ -40,9 +43,16 @@ while namecorrect == "no":
 if namecorrect == "yes":
     email = input("What is your email: ")
     emailcorrect = input("Is this correct (yes, no):"+email+":")
-    phone = input("What is your phone number (optional, type n/a if no number is available ): ")
-    correctphone = input("Is this correct (yes, no):"+ phone +":")
-    while correctphone == "yes":
+    while emailcorrect == "no":
+      email = input("What is your email: ")
+      emailcorrect = input("Is this correct (yes, no):" + email + ":")
+    while emailcorrect == "yes" and terminate == "false":
+      phone = input("What is your phone number (optional, type n/a if no number is available ): ")
+      correctphone = input("Is this correct (yes, no):"+ phone +":")
+      while correctphone == "no":
+        phone = input("What is your phone number *optional* ( type n/a if no number is available ): ")
+        correctphone = input("Is this correct (yes, no):"+ phone +":")
+      while correctphone == "yes" and terminate == "false":
         age = input("How old are you: ")
         sex = input("What is your sex (male, female, intersex, rather not say, other): ")
         if sex.lower() == "rather not say" or sex.lower() == "other" or sex.lower() == "intersex" or sex.lower() == "male" or sex.lower() == "female":
@@ -56,32 +66,36 @@ if namecorrect == "yes":
             pass
         gender = input(
               "what is your gender identity (man (not specifically trans or cis), women (not specifically trans or cis), transgender, non-binary, agender, genderfluid, genderqueer, other, rather not say): ")
+        while gender.lower() == "other":
+            gender = input("what is your gender identity: ")
         # Insert the users gender into the databse #
-        cursor.execute("INSERT INTO gender (gender) VALUES (?)", (gender,))
-        if gender.lower() not in ["man", "woman", "transgender", "non-binary", "agender","genderfluid", "genderqueer", "other", "rather not say"]:
-          print(gender)
-        else:
-          pass
+            cursor.execute("INSERT INTO gender (gender) VALUES (?)", (gender,))
+        sexuality = input ("what is your sexuality (straight, gay, lesbian, bisexual, pansexual, aspec (asexual spectrum), arospec (aromantic spectrum), queer, questioning,  other, rather not say :")
+        while sexuality.lower() == "other":
+            sexuality = input("what is your sexuality: ")
         hobbies = input("What are your Hobbies (separate by ','):")
         religous = input("Are you religious (yes, no): ")
         if religous == "no":
             print("OK")
             print("Thank You")
-            break
+            terminate = "true"
         if religous == "yes":
             religous = input("What religion do you identify with (christian, buddhist, muslim, jewish, other, rather not say): ")
             print("OK")
             print("Thank You")
-            break
+            terminate = "true"
         if religous == "other":
             print(input("what religion do you practice:"))
             print("OK")
             print("Thank You")
-            break
-
+            terminate = "true"
+      
 # Insert the information into the database #
 
         cursor.execute("INSERT INTO hobbies (hobbies) VALUES (?)", (hobbies,))
+        cursor.execute(
+          "INSERT INTO sexuality (sexuality) VALUES (?)", (sexuality,))
+        cursor.execute("INSERT INTO ethnicity (ethnicity) VALUES (?)", (ethnicity,))
         cursor.execute("INSERT INTO religion (religion) VALUES (?)", (religous,))
         cursor.execute("INSERT INTO age (age) VALUES (?)", (age,))
         cursor.execute("INSERT INTO sex (sex) VALUES (?)", (sex,))
